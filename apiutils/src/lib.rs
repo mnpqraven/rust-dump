a72359938cdbd8e4f1897c38f254fd4117895677
Othi <mnpq.raven@gmail.com>
2022-07-30 11:15:03 +0700

    clap, better error handing

--- apiutils/src/lib.rs
+++ apiutils/src/lib.rs
@@ -1,56 +1,54 @@
-use reqwest::{self, header::{CONTENT_TYPE, ACCEPT}};
-use serde::{Serialize, Deserialize};
-use std::fs;
 use json;
+use reqwest::{
+    self,
+    header::{ACCEPT, CONTENT_TYPE},
+};
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
+use tokio::fs as tokiofs;
 
 static TOKEN: &'static str = "token";
 
-// NOTE: conversion between json and struct
+// NOTE: De-/Serialization between json and rust struct
+// INFO: rename to change naming convention
+// #[serde(rename = "userId")]
+// user_id: i32
 #[derive(Debug, Serialize, Deserialize)]
 struct TokenChunk {
     code: i8,
-    result: Token
+    result: Token,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MarkerChunk {
     code: i8,
-    result: Vec<Marker>
+    result: Vec<Marker>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Marker {
     id: String,
-    lng: Option<f32>,
-    lat: Option<f32>,
     code: String,
     name: String,
     marker_id: String,
-    videos: Option<Vec<String>>,
-    tags: Option<Vec<String>>,
-    // hardcoded date for now
-    crated_at: Option<String>,
-    updated_at: Option<String>,
-    __text: Option<Vec<String>>
+    // __text: Option<Vec<String>>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Token {
-    token: String
+    token: String,
 }
-
-// INFO: rename to change naming convention
-// #[serde(rename = "userId")]
-// user_id: i32
 
 #[derive(Debug, Serialize, Deserialize)]
 struct UserAuth {
     account: String,
-    password: String
+    password: String,
 }
 
-pub async fn auth() -> Result<String, reqwest::Error>{
-    let host = fs::read_to_string("private-dump/login-auth").expect("can't find host file containing the auth url\nmake sure the file exists in ./private-dump");
+pub async fn auth() -> Result<String, reqwest::Error> {
+    let host = fs::read_to_string("private-dump/login-auth").expect(
+        "can't find host file containing the auth url\nmake sure the file exists in ./private-dump",
+    );
     let client = reqwest::Client::new();
 
     let cred_file = fs::read_to_string("private-dump/user_auth.json")
@@ -59,49 +57,70 @@ pub async fn auth() -> Result<String, reqwest::Error>{
 
     let user = UserAuth {
         account: cred_obj["account"].to_string(),
-        password : cred_obj["password"].to_string()
+        password: cred_obj["password"].to_string(),
     };
 
-    let response: TokenChunk = client.post(host)
+    let response: TokenChunk = client
+        .post(host)
         .header(CONTENT_TYPE, "application/json")
         .header(ACCEPT, "application/json")
         .json(&user)
         .send()
         .await?
         .json()
-        .await?;
+        .await
+        .expect("not a valid request, double check your url and your json payload");
 
-    // response should be the token
-    // INFO: debug
-    // println!("{:?}", response);
-    // INFO: debug
-    // println!("{}", token);
+    // TODO: test
+    tokiofs::write("private-dump/token", response.result.token.clone())
+        .await
+        .expect("error writing to file");
 
-    let token = response.result.token.clone();
-    Ok(token)
+    Ok(response.result.token)
 }
 
 // TODO: work
-pub async fn markerss() -> Result<(), reqwest::Error>{
-    let host = fs::read_to_string("private-dump/pointss-host").expect("can't find pointss-host file containing the auth url\nmake sure the file exists in ./private-dump");
+pub async fn markerss() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
-    let token_val = auth().await.expect("authentication failed");
-    let response: MarkerChunk = client.get(host)
+
+    // TODO: make fs token handler
+    let host = std::fs::read_to_string("private-dump/markerss-host")
+        .expect("can't find pointss-host file containing the auth url\nmake sure the file exists in ./private-dump");
+    let token_val: String;
+    if Path::new("private-dump/token").exists() {
+        println!("using token file ...");
+        token_val = std::fs::read_to_string("private-dump/token").expect("can't open file");
+    } else {
+        println!("not token file found, submitting user authentication");
+        token_val = auth().await.expect("authentication failed");
+    }
+
+    let response = client
+        .get(host)
         .header(TOKEN, token_val)
         .header(CONTENT_TYPE, "application/json")
         .header(ACCEPT, "application/json")
         .send()
-        .await?
-        .json()
-        .await?;
+        // TODO: handle invalid token here
+        .await;
+    if response.is_err() {
+        println!("invalid token file, reauthing");
+        // for now
+        auth().await.expect("cannot reauth");
+        println!("reauth attempted, rerun the program\nif it still doesn't work, delete the token");
+        // TODO: resend request
+    }
 
-    for item in response.result {
-        println!("id: {:?} name: {:?} code: {:?}", item.id, item.name, item.code);
+    let data: MarkerChunk = response.unwrap().json().await?;
+    for item in data.result {
+        println!(
+            "id: {:?} name: {:?} code: {:?}",
+            item.id, item.name, item.code
+        );
     }
     // println!("{:?}", response);
     Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
--- apiutils/src/main.rs
+++ apiutils/src/main.rs
@@ -1,11 +1,46 @@
 use apiutils::{auth, markerss};
+// use apiutils::auth;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, long_about = None)]
+#[clap(about = "api data fetcher and eventually data manipulation tool")]
+struct Args {
+    /// Number of times to greet
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    count: u8,
+
+    /// API endpoints, currently 'auth', 'nakedauth' and 'markerss' are available
+    #[clap(short, long, value_parser)]
+    prop: String,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error>{
-    let token = auth()
-        .await.expect("authentication error");
-    println!("main getting token: {}", token);
+async fn main() -> Result<(), reqwest::Error> {
+    // TODO: clear token writer (on user demand only ?)
+    // WARNING: lower security, run this locally + isolated only
 
-    markerss().await.expect("error getting markerss, check lib.rs");
+    // INFO: CLI with Clap
+    let args = Args::parse();
+
+    // TODO: enumerate ?
+    println!("{}", args.count);
+
+    if args.prop == "markerss" {
+        markerss()
+            .await
+            .expect("error getting markerss, check lib.rs");
+    }
+
+    // TODO: cleaner
+    if args.prop == "auth" {
+        auth().await.expect("authentication error");
+        println!("authentication completed, generated token");
+    }
+    if args.prop == "nakedauth" {
+        let naked_token = auth().await.expect("authentication error");
+        println!("{}", naked_token);
+    }
+
     Ok(())
 }
