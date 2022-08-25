febcc81ba3ce0b721d8574c9e1b80bd405d81a91
Othi <mnpq.raven@gmail.com>
2022-08-25 09:18:48 +0700

    naswiz refactoring

--- naswiz/src/lib.rs
+++ naswiz/src/lib.rs
@@ -18,14 +18,14 @@ impl NAS {
 
 static WHAT_RE: &'static str = r"(What\s*=\s*//)(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))";
 #[allow(dead_code)]
-static NAME_RE: &'static str = r"^(.*)/([a-zA-Z-]*.mount)$";
+static SYSTEMD_PATH: &'static str = "/etc/systemd/system/";
 
 /// replaces the mountpoint file, changing the ip address in the what field
 ///
 /// * `mountpoint`: path of the mountpoint file
 /// * `new_ip`: ip to replace
 pub fn gen_new_file(mount: String, new_ip: Ipv4Addr) -> Result<(), &'static str> {
-    let mountpoint = format!("/etc/systemd/system/{}.mount", mount);
+    let mountpoint = format!("{}{}.mount", SYSTEMD_PATH, mount);
     let tmp_mountpoint = format!("/tmp/{}.mount", mount);
     let content = String::from(fs::read_to_string(&mountpoint).unwrap());
     let mut result = String::new();
@@ -47,8 +47,6 @@ pub fn gen_new_file(mount: String, new_ip: Ipv4Addr) -> Result<(), &'static str>
     let mut file = File::create(&tmp_mountpoint).expect("can't create a new file");
     file.write_all(result.as_bytes())
         .expect("can't write to file");
-    println!("copying with sudo cp {} {}", &tmp_mountpoint, &mountpoint);
-
     copy_to_systemd(mount);
     Ok(())
 }
@@ -60,7 +58,7 @@ fn copy_to_systemd(mount: String) {
     let child = Command::new("sudo")
         .arg("cp")
         .arg(format!("/tmp/{}.mount", mount))
-        .arg("/etc/systemd/system")
+        .arg(SYSTEMD_PATH)
         .stdin(Stdio::inherit())
         .output() // NOTE: importand for catching stdin
         .expect("failed to run copy command");
@@ -95,21 +93,6 @@ pub fn lookup(domain: String) -> Result<Ipv4Addr, std::net::AddrParseError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn regex() {
-        let mountpoint = "/etc/systemd/system/media-nasremote-music.mount";
-        let name_regex = Regex::new(NAME_RE).unwrap();
-        let captures = name_regex.captures(mountpoint).unwrap();
-        assert_eq!(
-            captures.get(1).map_or("", |m| m.as_str()),
-            "/etc/systemd/system"
-        );
-        assert_eq!(
-            captures.get(2).map_or("", |m| m.as_str()),
-            "media-nasremote-music.mount"
-        );
-    }
 
     #[test]
     fn look() {
